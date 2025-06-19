ab67e2f0cb8ff4dc5cebbe933b623259d49df132
arturnovais <artureta99@gmail.com>
2025-06-19 15:58:29 -0300

    feat: ajustando estrutura de docker

--- src/interface/app.py
+++ src/interface/app.py
@@ -1,1 +1,10 @@
-#Implementar
+import streamlit as st
+
+st.set_page_config(
+    page_title="EchoLoco",
+    page_icon=":microphone:",
+    layout="centered"
+)
+
+st.title("EchoLoco")
+st.markdown("Bem-vindo ao sistema EchoLoco! Use o menu lateral para navegar entre as funções.")
